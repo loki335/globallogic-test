c131102631e8290cbf52d5011747e594824d4911
loki335 <muddapolulokesh@gmail.com>
2024-08-05 19:28:34 +0530

    Update globallogic.py

--- globallogic.py
+++ globallogic.py
@@ -3,7 +3,8 @@ import json
 import time
 
 # send /get request and validate response code
-base_url = "https://httpbin.org"
+base_url_get = "https://httpbin.org"
+base_url_post = "https://httpbin.org/post"
 get_response = requests.get(base_url)
 assert get_response.status_code == 200
 f"GET request failed with status code {get_response.status_code}"
@@ -11,7 +12,7 @@ f"GET request failed with status code {get_response.status_code}"
 #send /post request with json body and validate response contain relevant data
 data = {"type":"new", "valid":True}
 headers = {"Content-Type": "application/json"}
-post_response =requests.post(base_url, data=json.dumps(data), headers=headers).json()
+post_response =requests.post(base_url_post, data=json.dumps(data), headers=headers).json()
 assert post_response["type"] is "new", "response data doesn't match"
 
 #validate request with delayed response delay/{delay_time}
@@ -24,7 +25,7 @@ assert (response_time - delay_time) < 1, "response exceeds the allowed delay tim
 #validate any negative scenario
 invalid_data = {"valid":False} #invalid data
 try:
-    invalid_response = requests.post(base_url, data=json.dumps(invalid_data), headers=headers).json()
+    invalid_response = requests.post(base_url_post, data=json.dumps(invalid_data), headers=headers).json()
     assert False, "request with invalid data succeded"
 except requests.exceptions.RequestException as e:
     print(e)
